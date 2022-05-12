ed58ae9ed95f7a5ea90983e735c2723dde6fd088
mohd <mohdahteshamsap72786@gmail.com>
2022-05-12 13:54:09 +0530

    Better error handling

--- sale/models.py
+++ sale/models.py
@@ -3,10 +3,13 @@ from django.db import models
 # Create your models here.
 class Student(models.Model):
     membership_no = models.CharField(max_length=15)
-    form_data = models.CharField(max_length=10)
+    form_no = models.CharField(max_length=20,default=0)
+    form_date = models.DateField(null=True)
+    name = models.CharField(max_length=200,default='')
     guardian_name = models.CharField(max_length=200)
     mothers_maiden_name = models.CharField(max_length=200)
-    phone = models.BigIntegerField(unique=True)
+    dob = models.DateField(null=True)
+    phone = models.IntegerField(null=True)
     email = models.CharField(max_length=100)
     gender = models.CharField(max_length=6)
     educational_qualification = models.CharField(max_length=50)
--- sale/views.py
+++ sale/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponse, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 #from home.models import Contact
 import os
 from django.contrib import messages
@@ -15,9 +15,12 @@ def index(request):
 def student(request):
     if request.method=='POST':
         membership_no=request.POST['membership_no']
-        form_data=request.POST['form_data']
+        form_no=request.POST['form_no']
+        form_date=request.POST['form_date']
+        name=request.POST['name']
         guardian_name=request.POST['guardian_name']
         mothers_maiden_name=request.POST['mothers_maiden_name']
+        dob=request.POST['dob']
         phone=request.POST['phone']
         email=request.POST['email']
         gender=request.POST['gender']
@@ -26,11 +29,28 @@ def student(request):
         share_count=request.POST['share_count']
         introduced_by=request.POST['introduced_by']
         witness=request.POST['witness']
-        ts=Student(membership_no=membership_no,form_data=form_data,guardian_name=guardian_name, mothers_maiden_name= mothers_maiden_name,
-                   phone= phone,email=email,gender=gender,educational_qualification=educational_qualification,membership_fee=membership_fee,
-                   share_count=share_count,introduced_by=introduced_by,witness=witness)
+        
+        '''ts=Student(membership_no=membership_no,form_no=form_no,form_date=form_date,name=name,guardian_name=guardian_name,mothers_maiden_name=mothers_maiden_name,
+                    dob=dob,phone=phone,email=email,gender=gender,educational_qualification=educational_qualification,membership_fee=membership_fee,
+                    share_count=share_count,introduced_by=introduced_by,witness=witness)
         ts.save()
         return HttpResponse('OK')
     else:
         #return HttpResponse('Password didnot match')
-        return render(request,'sale/student.html')
+        return render(request,'sale/student.html')'''
+        
+        existingStudent = Student.objects.filter(phone=phone)
+        if existingStudent.exists() :
+            return JsonResponse({'status':"error","message":"Phone number already registered"},status=400)
+              
+        try :
+            ts = Student(membership_no=membership_no,form_date=form_date,guardian_name=guardian_name,mothers_maiden_name=mothers_maiden_name,
+                   phone=phone,email=email,gender=gender,educational_qualification=educational_qualification,membership_fee=membership_fee,
+                   share_count=share_count,introduced_by=introduced_by,witness=witness)
+            ts.save()
+            return redirect('student')
+        except Exception as e:
+            errMessage = "Unable to save to db" + str(e)
+            return JsonResponse({'status':"error","message": errMessage }, status=400)
+
+    return render(request,'sale/student.html')
